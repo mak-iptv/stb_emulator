cf45b25734af358adafa7be9cdd0fd75b402161a
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2025-10-31 22:07:26 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
+  build: { outDir: "dist" },
   server: {
     port: 3000,
     proxy: {
-      '/mag-proxy': {
-        target: 'http://localhost:10000', // porta e backend-it Express gjatë zhvillimit
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/mag-proxy/, '/mag-proxy')
+      "/mag-proxy": {
+        target: "http://localhost:10000",
+        changeOrigin: true
       }
     }
   }
-})
+});
